487b604ed4beb6483050c78ed41a227874a4df95
Erfan Saatchi <112129390+accelangel@users.noreply.github.com>
2023-03-25 16:54:15 -0400

    make a function that asks that prompts the player for a choice
    the function should be case-insensitive as well as ask the
    player to try again if they type an invalid choice other
    than rock, paper or scissors

--- source.js
+++ source.js
@@ -7,7 +7,7 @@ return rock for 1, paper for 2 or scissors for 3
 /*This function returns the computers choice by picking a random
 number between 1 and 3, and returning either rock/paper/scissors 
 based on the value of randomNum 1/2/3, respectively.*/
-function getComputerChoice() { 
+function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3) + 1;
     if (randomNum == 1) {
         return 'rock';
@@ -19,7 +19,21 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
-
-console.log(getComputerChoice());
-
-
+/*
+Declare a function called getPlayerChoice
+create a variable that stores the player choice from a prompt
+check if the playerInput is either rock, paper or scissors
+return the player's choice
+it should be case-insensitive
+if the choice is invalid, let the player know to try again
+*/
+function getPlayerChoice() {
+    let playerInput = prompt('Enter Rock, Paper or Scissors');
+    playerInput = playerInput.toLowerCase();
+    if (playerInput == 'rock' || playerInput == 'paper' || playerInput == 'scissors') {
+        return playerInput;
+    }
+    else {
+        console.log('Invalid choice, please try again');
+    }
+}
