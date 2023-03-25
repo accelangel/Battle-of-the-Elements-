67932a9c69be034ae71666d0f5efa3a38d37e30d
Erfan Saatchi <112129390+accelangel@users.noreply.github.com>
2023-03-25 16:59:24 -0400

    Create a function that plays a round of RPS
    The function will take the arguments of playerSelection and
    computerSelection and declare a winner depending on the
    respective matchup. a console.log of playround outputs the
    result of the round in the console.

--- source.js
+++ source.js
@@ -37,3 +37,46 @@ function getPlayerChoice() {
         console.log('Invalid choice, please try again');
     }
 }
+/*
+declare a function called playRound() with paramters for player and computer
+if playerSelection is rock and computerChoice is scissors, player wins
+if playerSelection is rock and computerChoice is paper,  player loses
+
+if playerSelection is paper and computerChoice is rock, player wins
+if playerSelection is paper and computerChoice is scissors, player loses
+
+if playerSelection is scissors and computerChoice is paper, players wins
+if playerSelection is scissors and computerChoice is rock, player loses
+else the playerSelection must be the same as computerChoice, tie game
+*/
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'Rock beats Scissors, you win!';
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'Paper beats Rock, you lose.';
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'Paper beats Rock, you win!';
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'Scissors beats paper, you lose.';
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return 'Scissors beats paper, you win!';
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'Rock beats scissors, you lose.';
+    }
+    else {
+        return 'Tie game';
+    }
+}
+//declare two variables to store the player and computer selection
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+//log both selections in the console
+console.log(`you chose ${playerSelection}`);
+console.log(`the computer chose ${computerSelection}`);
+//log the return of playround() which requires the player/comnputer selection arguments to work
+console.log(playRound(playerSelection,computerSelection));
